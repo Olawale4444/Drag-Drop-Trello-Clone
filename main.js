b10eb20524123f1d4f9a7ce434f9d8eda6986d10
Olowonyo Olawale <31905212+Olawale4444@users.noreply.github.com>
2020-01-19 23:47:54 +0100

    Update main.js
    Added comments and some functionality

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
 const list_items= document.querySelectorAll('.listitems');
-const list = document.querySelectorAll('.list');
+const lists = document.querySelectorAll('.list1');
 
-var draggedItem = null;
+let draggedItem = null;
 
 for (let b =0; b <list_items.length; b++){
 	const item = list_items[b];
 
+	//Listens for a drag event and turns The draggable item invisible, while it's on the list.
 	item.addEventListener('dragstart', function(){
-		console.log('Dragstart Alert');
 		draggedItem= item;
 
 		setTimeout (function () {
 		item.style.display ='none';	
-		}, 0)
-		
+		}, 0.1)
 	});
 
-	item.addEventListener('dragend', function(){
+	//Listens for the end of the drag event and converts the draggable 
+	//item back to null since it's been dropped on the list.
 
-		console.log('Dragend Alert');
-		draggedItem = null;
+	item.addEventListener('dragend', function(){
+		setTimeout(function() {
+			draggedItem.style.display = 'block';
+
+			draggedItem = null;
+
+		}, 0.1);
 	})
 
-	for (let i =0; i<list.length; i++){
-		const list = list[i];
+	for (let i =0; i < lists.length; i++){
+		const list = lists[i];
+
+		// Listens for a dragover event by the draggable item. i.e. when the draggable item
+		// is moved over a list, Empty or not 
+		list.addEventListener('dragover', function(eListener){
+			eListener.preventDefault();
+		});
+
+		list.addEventListener('dragenter', function(eListener){
+			eListener.preventDefault();
+			this.style.backgroundColor = 'rgba (0, 0, 0, 0.22)';
+		});
+
+
+		list.addEventListener('dragleave', function(eListener){
+			this.style.backgroundColor = 'rgba (0, 0, 0, 0.11)';
+			});
+
+
+		list.addEventListener('drop', function(eListener){
+			console.log('drop');
+			this.append(draggedItem);
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.11)';
+		});
+
 	}
 } 
